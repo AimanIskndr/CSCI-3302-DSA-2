ab8f1315019b3682110ed7204113659dfd22fe77
Aiman Iskandar <esxkandar14@gmail.com>
2024-04-23 12:23:57 +0800

    Update activity6a.py

--- Greedy and Dynamic Programming/activity6a.py
+++ Greedy and Dynamic Programming/activity6a.py
@@ -1,12 +1,14 @@
+memo = {}
 coins = [5, 1, 2, 10, 6, 2]
 
-def pick_up(i):
+def F(n):
     
-    if i >= len(coins):
+    if n < 0:
         return 0
+    if n in memo:
+        return memo[n]
     
-    best = 0
-    best = max(pick_up(i+3) + coins[i], pick_up(i+2), pick_up(i+1))
+    best = max(F(n-3) + coins[n], F(n-2), F(n-1))
     return best
 
-print(pick_up(0)) #output : 15
+print(F(len(coins) - 1)) #output : 15
